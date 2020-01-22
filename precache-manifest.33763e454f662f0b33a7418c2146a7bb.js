5cf5cb2e4bc3196fae4a573f3d25fe253ce84723
sanchita30 <42735173+sanchita-kadge-adrosonic@users.noreply.github.com>
2020-01-22 17:09:03 +0530

    first

---
+++ precache-manifest.33763e454f662f0b33a7418c2146a7bb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "893fa811e2421f763628fbba297b7cbd",
+    "url": "/static/media/dummy.893fa811.jpg"
+  },
+  {
+    "revision": "74f136b6d27758aff689",
+    "url": "/static/css/main.8e754d54.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "893fa811e2421f763628fbba297b7cbd",
+    "url": "/static/media/homeSec3.893fa811.jpg"
+  },
+  {
+    "revision": "2e20f0932959924c16fd",
+    "url": "/static/js/2.72e79b0c.chunk.js"
+  },
+  {
+    "revision": "4b04172b672c5652f77e275d71c98a2e",
+    "url": "/static/media/TataTrustsNewLogo.4b04172b.png"
+  },
+  {
+    "revision": "74f136b6d27758aff689",
+    "url": "/static/js/main.6f6a6dcc.chunk.js"
+  },
+  {
+    "revision": "637046d6cab3d7c109a88c5c520d7e4b",
+    "url": "/static/media/Doc.637046d6.PNG"
+  },
+  {
+    "revision": "b03b24ad2dcf64c2954225b1ce74e14f",
+    "url": "/static/media/Patient.b03b24ad.PNG"
+  },
+  {
+    "revision": "a8ae2151e93d484a1ce7969db59d606e",
+    "url": "/static/media/Trainee.a8ae2151.PNG"
+  },
+  {
+    "revision": "2e20f0932959924c16fd",
+    "url": "/static/css/2.970138de.chunk.css"
+  },
+  {
+    "revision": "fb422c4d2a0cfa76838ca3da09561039",
+    "url": "/index.html"
+  }
+];
